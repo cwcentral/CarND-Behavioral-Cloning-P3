68f485ab421e7a162f298b30a772b3b35b82a6ad
cwcentral <cwcentral@gmail.com>
2018-03-26 23:21:02 -0700

    update notes

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 #
 # P3 Behaviorial cloning
+# Best run on a 1080 GPU
+# 75% success chance on 760GTX mobile or less.
 #
 import os
 import random
@@ -73,10 +75,14 @@ def lenet_model():
 
 #
 # Nvidia model as described in Udacity lesson
+# https://devblogs.nvidia.com/deep-learning-self-driving-cars/
+# https://arxiv.org/pdf/1604.07316v1.pdf
 #
 def nvidia_model():
 	model = Sequential()
 	model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+	#
+	# Crop the image to get rid of the sky and most tall trees.
 	model.add(Cropping2D(cropping=((47,0),(0,0))))
 	model.add(Convolution2D(24, 5, 5, subsample=(2,2),activation='relu'))
 	model.add(Convolution2D(36, 5, 5, subsample=(2,2),activation='relu'))
@@ -85,8 +91,9 @@ def nvidia_model():
 	model.add(Convolution2D(64, 3, 3, activation='relu'))
 	model.add(Flatten())
 	model.add(Dense(100))
+	#model.add(Dropout(0.6))  # never worked right in practice
 	model.add(Dense(50))
-	#model.add(Dropout(0.6))
+	#model.add(Dropout(0.6))  # never worked right in practice
 	model.add(Dense(10))
 	model.add(Dense(1))
 	return model
@@ -95,7 +102,7 @@ def nvidia_model():
 # preprocess images
 #
 def process_image(img):
-	# possible resize vgg, nvidia, alexnet
+	# possible resize vgg, alexnet
 	#img = cv2.resize(img,(200, 66), interpolation = cv2.INTER_AREA)
 
 	# convert to YUV as nvidia paper describes
@@ -104,6 +111,7 @@ def process_image(img):
 
 #
 # Load generator to prevent out of memory
+# Based on the suggestion in the project videos
 #
 def generator(samples, batch_size=128):
     num_samples = len(samples)
@@ -125,30 +133,38 @@ def generator(samples, batch_size=128):
                 left_current_path = './sim_data/IMG/' + left_path.split('/')[-1]
                 right_current_path = './sim_data/IMG/' + right_path.split('/')[-1]
 
+		# exploit the use of left and right images recorded
+		# use the recommended 0.2 delta in camera view related to steering angle
                 steering_center = float(batch_sample[3])
                 correction = 0.2
                 steering_left = steering_center + correction
                 steering_right = steering_center - correction
 
+		# add augmented data
                 images.extend([process_image(cv2.imread(current_path)), 
                 		process_image(cv2.imread(left_current_path)),
                 		process_image(cv2.imread(right_current_path))])
 
                 measurements.extend([steering_center, steering_left, steering_right])
 
+	    # Augment with MORE data
             # trim image to only see section with road
             augmented_images = []
             augmented_measurements = []
             for (im, meas) in zip(images, measurements):
                 augmented_images.append(im)
                 augmented_measurements.append(meas)
+
+		# Flip as suggested by lesson
                 augmented_images.append(cv2.flip(im,1))
                 augmented_measurements.append(-1.0*meas)
 
+		# blur
                 blur_in = cv2.GaussianBlur(im, (5,5), 30.0)
                 augmented_images.append(blur_in)
                 augmented_measurements.append(meas)
 
+		# rotate
                 rot = random.randint(-30, 30)
                 rows,cols,dep = im.shape
                 M = cv2.getRotationMatrix2D((cols/2,rows/2),rot,1)
@@ -156,41 +172,46 @@ def generator(samples, batch_size=128):
                 augmented_images.append(np.resize(im, (160,320,3)))
                 augmented_measurements.append(meas)
 
-
+	    # assign and shuffle
             X_train = np.array(augmented_images)
             Y_train = np.array(augmented_measurements)
             yield sklearn.utils.shuffle(X_train, Y_train)
 	
+# Debug Helper
 def show_histogram(dataset, bins):
     plt.figure(figsize=(5,5))
     plt.hist(dataset, bins)
 
+# Debug Helper
 def show_image(img):
 	cv2.imshow('ImageOutput', img)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
 #
-# BEGIN
+# BEGIN main
 #
+
+# Get all measurements
 lines = []
 with open('./sim_data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
 
-# read measurements and show a histogram
+# TODO read measurements and show a histogram
 
-# Split the sample csv into test and validation sets
+# Split the sample csv into test and validation set at 80/20
 print(len(lines))
 train_samples, validation_samples = train_test_split(lines, test_size=0.2)
 
-# create generators
+# create generators as recommended in rubric
 train_generator = generator(train_samples, batch_size=16)
 validation_generator = generator(validation_samples, batch_size=16)
 
 # Choose your model/architecture
 model = nvidia_model()
+
 #model = lenet_model()
 #model = vgg_model()
 
@@ -198,8 +219,6 @@ model = nvidia_model()
 model.compile(loss='mse', optimizer='adam')
 
 # Run it
-print (len(train_samples))
-
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, 
             nb_val_samples=len(validation_samples), nb_epoch=7) 
 
@@ -212,6 +231,7 @@ print("saved")
 print(history_object.history.keys())
 
 # plot the training and validation loss for each epoch and compare fitting
+# as explained in lesson
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
 plt.title('model mean squared error loss')
